bb17b79fec28c77305091f636a4066c86c120613
Rebekah Daniels <math919191@gmail.com>
2023-01-21 11:27:26 -0700

    started on express and node

---
+++ simon/react-simon/src/App.js
@@ -0,0 +1,13 @@
+import playSimon from './playSimon.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello world</p>
+      <playSimon/>
+    </div>
+  );
+}
+
+export default App;
