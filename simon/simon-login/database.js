f0f3d7ff6f82393d443221d06de0184906d333cf
Rebekah Daniels <math919191@gmail.com>
2023-02-06 12:27:59 -0700

    got simon-login working!

--- simon/simon-login/database.js
+++ simon/simon-login/database.js
@@ -1,4 +1,7 @@
 const {MongoClient} = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
 
 const userName = process.env.MONGOUSER;
 const password = process.env.MONGOPASSWORD;
@@ -12,6 +15,32 @@ const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 
 const client = new MongoClient(url);
 const scoreCollection = client.db('simon').collection('score');
+const userCollection = client.db('simon').collection('user');
+
+
+function getUser(email){
+    return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token){
+    return userCollection.findOne({ token: token });
+}
+
+
+async function createUser(email, password){
+    //hash the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+    
+    return user;
+}
+
 
 function addScore(score){
     scoreCollection.insertOne(score);
@@ -26,7 +55,12 @@ function getHighScores(){
     const cursor = scoreCollection.find(query, options);
     return cursor.toArray() 
 }
-let example = {name : "playerName", score: 10, date: "12/12/12" } 
-addScore(example)
 
-module.exports = { addScore, getHighScores };
+
+module.exports = { 
+    addScore, 
+    getHighScores, 
+    createUser, 
+    getUser, 
+    getUserByToken 
+};
--- simon/simon-login/public/js/login.js
+++ simon/simon-login/public/js/login.js
@@ -1,13 +1,18 @@
-function login(){
-    console.log("logging in.")
-    const nameEl = document.querySelector("#name")
-    localStorage.setItem("userName", nameEl.value)
-    window.location.href = "play.html"
-}
 
-
+//checks if the user is logged in and sets the display
 (async () => {
-    let authenticated = true;
+    let authenticated = false;
+    
+    const userName = localStorage.getItem('userName');
+    
+    if (userName) {
+        //sets the username field to the username in local storage
+        const nameEl = document.querySelector('#userName');
+        // nameEl.value = userName;
+        const user = await getUser(nameEl?.value);
+        authenticated = user?.authenticated;
+    }
+
     if (authenticated == false){
         setDisplay("playControls", 'none')
         setDisplay("loginControls", 'block')    
@@ -15,13 +20,74 @@ function login(){
         setDisplay("playControls", 'block')
         setDisplay("loginControls", 'none')
     }
-    console.log("running")
 })();
+
+
+
+async function loginUser() {
+    loginOrCreateUser(`/api/auth/login`);
+}
+  
+async function createUser() {
+    loginOrCreateUser(`/api/auth/create`);
+}
+
+async function loginOrCreateUser(endpoint){
+    const email = document.querySelector("#username")?.value;
+    const password = document.querySelector("#password")?.value;
+
+    const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify(
+            {email: email, password: password}
+        ),
+        headers: {'Content-type': 'application/json; charset=UTF-8'}
+    })
+
+    const body = await response.json();
+
+    if (response?.status == 200){
+        localStorage.setItem('userName', email);
+        window.location.href = 'play.html';
+    } else {
+        const modalEl = document.querySelector('#error');
+        // modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+        //modalEl.textContent = `⚠ Error: ${body.msg}`;
+        modalEl.innerHTML = "error..."
+        ///onst msgModal = new bootstrap.Modal(modalEl, {});
+        //msgModal.show();
+      }
+
+}
+
+function play() {
+    window.location.href = 'play.html';
+}
+
+function logout() {
+    fetch(`/api/auth/logout`, {
+        method: 'delete',
+    }).then(() => (window.location.href = '/'));
+
+    localStorage.clear();
+
+}
+
+async function getUser(email) {
+    // See if we have a user with the given email.
+    const response = await fetch(`/api/user/${email}`);
+    if (response.status === 200) {
+      return response.json();
+    }
+  
+    return null;
+}
+
 
 
 function setDisplay(controlId, display) {
     const playControlEl = document.querySelector(`#${controlId}`);
     if (playControlEl) {
-      playControlEl.style.display = display;
+        playControlEl.style.display = display;
     }
-  }
+}
