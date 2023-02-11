ea4c3c1a2add82ce8e960717e1f04e00104d12da
Rebekah Daniels <math919191@gmail.com>
2023-02-11 09:04:04 -0700

    basic game play works!

--- StartUp/project/script.js
+++ StartUp/project/script.js
@@ -2,7 +2,8 @@
 class Game {
     constructor(){
         this.board = [];
-        this.playerTurn = true;
+        this.playerTurn = "yellow";
+        this.initBoard()
     }
 
     startGame(){
@@ -20,11 +21,56 @@ class Game {
 
 
     //helper functions
-    clearBoard(){}
-    initBoard(){}
+    clearBoard(){
+        for (let i = 0; i < 7; i++){
+            for (let j = 0; j < 6; j++){
+                this.board[i][j] = "blank";
+            }   
+        }
+    }
+
+    initBoard(){
+        for (let i = 0; i < 7; i++){
+            this.board.push([])
+            for (let j = 0; j < 6; j++){
+                this.board[i].push("blank");
+            }   
+        }
+    }
+
+    updateBoard(colNum, color){
+        
+        for (let j = 6; j >= 0; j--){
+            if (this.board[colNum][j] == "blank"){
+                this.board[colNum][j] = color;
+                this.changeDotColor(j,colNum,color)
+                break;
+            }
+        }   
+    }
+
     checkColNumFull(colNum){}
     
+    changeDotColor(rowNum, colNum, color){
+        const dot = document
+                    .querySelector(`.row${rowNum+1}`)
+                    .querySelector(`.col${colNum+1}`)
 
+        dot.classList.add(`${color}Dot`)
+        
+    }
+    rowClicked(rowNum){
+        this.updateBoard(rowNum, this.playerTurn)
+        this.playerTurn = (this.playerTurn == "red") ? "yellow" : "red" ;
+        
+    }
 
 }
 
+
+var game = new Game();
+
+function rowClick(rowNum){
+    game.rowClicked(rowNum)
+    console.log(rowNum)
+}
