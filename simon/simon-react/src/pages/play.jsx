63a961201b257ae094a6211bb872450bccfa8931
Rebekah Daniels <math919191@gmail.com>
2023-02-21 16:16:16 -0700

    additional game logic

--- simon/simon-react/src/pages/play.jsx
+++ simon/simon-react/src/pages/play.jsx
@@ -3,13 +3,21 @@ import { Players } from './playElements/players';
 import "../css/play.css"
 import { SimonButton } from './playElements/simonButton';
 import { GameControls } from './playElements/gameControls';
+import { getRandomColor, saveScore, delay } from './playElements/play';
 
 
 export function Play(props) {
 
   const [score, setScore] = useState(0);
-  const [sequence, setSequence] = useState([]);
-  const [currNumInSeq, setCurrNumInSeq] = useState(0);
+  // const [sequence, setSequence] = useState([]);
+  // const [currNumInSeq, setCurrNumInSeq] = useState(0);
+  const [gameIsOver, setGameIsOver] = useState(false);
+
+  let sequence = [];
+  function setSequence(seq){sequence = seq}
+  let currNumInSeq = 0;
+  function setCurrNumInSeq(num){currNumInSeq = num}
+
 
   const greenButton = useRef();
   const redButton = useRef();
@@ -35,24 +43,69 @@ export function Play(props) {
     }
   }
 
+  async function flashButton(color){
+    console.log(color);
+    await getButtonFromColor(color).current.flash();
+
+  }
+
   function reset(){
-    console.log('reset')
+    startGame();
+    // if (!gameIsOver){
+    //   startGame();
+    // } else {
+    //   console.log("finish the game")
+    // }
   }
 
   function onButtonClick(color){
-    getButtonFromColor(color).current.flash();
-    console.log(color)
+    
+    flashButton(color);
+    
+    if (color != sequence[currNumInSeq]){
+      endGame();
+    } else {
+
+        if (currNumInSeq+1 == sequence.length){
+          setCurrNumInSeq(currNumInSeq => currNumInSeq+1);
+          playRound();
+          setScore(score+1);
+        }
+    }
   }
 
   function startGame(){
+    setGameIsOver(false);
     setScore(0);
     setSequence([]);
     setCurrNumInSeq(0);
+    playRound();
+
   }
 
   function endGame(){
-    
+    saveScore(score)
+    setGameIsOver(true);
+    alert("Game over, click reset to play again")
   }
+
+  async function playRound(){
+    let newSeq =  [...sequence, getRandomColor()];
+    setSequence(newSeq);
+    setCurrNumInSeq(0);
+    await delay(1000);
+    console.log("playing sequence")
+    await playSequence(newSeq)
+  }
+
+  async function playSequence(sequence){
+    console.log(sequence)
+    for (let i = 0; i < sequence.length; i++){
+      await flashButton(sequence[i]);
+      console.log("flashed ", sequence[i])
+    }    
+  }
+
 
   return (
   <div className="content">
--- simon/simon-react/src/pages/playElements/play.js
+++ simon/simon-react/src/pages/playElements/play.js
@@ -11,4 +11,39 @@ function delay(milliseconds) {
     });
 }
 
-export {getRandomColor, delay }
+async function saveScore(score){
+  const playerName = localStorage.getItem('userName');
+  const date = new Date().toLocaleDateString();
+
+  let gameScore = {name : playerName, score: score, date: date }
+  let currScores = JSON.parse(localStorage.getItem('scores'));
+  
+  try {
+    const response = await fetch('/api/score', {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(gameScore),
+    });
+
+    // Store what the service gave us as the high scores
+    const scores = await response.json();
+    localStorage.setItem('scores', JSON.stringify(scores));
+  } catch {
+    // If there was an error then just track scores locally
+    if (currScores){
+        for (let i = 0; i < currScores.length; i++) {
+            if (currScores[i].score <= this.score){
+                currScores.splice(i, 0, gameScore);
+                break;
+            }
+        }
+        localStorage.setItem("scores", JSON.stringify(currScores))
+    } else {
+        let array = []
+        array.push(gameScore)
+        localStorage.setItem("scores", JSON.stringify(array))
+    }
+  }
+}
+
+export {getRandomColor, delay, saveScore }
